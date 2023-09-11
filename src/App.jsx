0081efc624c2c049690af5e6bc1517bc387d6a2b
Ryww0 <laurent.chaitas@gmail.com>
2023-09-11 15:03:50 +0200

    v1.23 - a::padding inline 0 in sm

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
   };
 
   return (
-    <div className="container-fluid px-5">
+    <div className="container-fluid px-sm-5">
       <Router>
         <Header
           toggleAbout={toggleAbout}
